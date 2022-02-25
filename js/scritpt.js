007927ed0d797c307de2fc55023bb97a9a42c696
Daniel Marelli <marelli_11@hotmail.com>
2022-02-24 23:45:25 -0300

    Add files via upload

---
+++ js/scritpt.js
@@ -0,0 +1,78 @@
+ /*$(document).ready(function(){
+
+    $('#github').on('mouseover', fnCambiaBtn);
+    $('#git').on('mouseout', fnCambiaBtn2);
+    $('#whatsapp').on('mouseover', fnCambiaBtn);
+    $('#wsp').on('mouseout', fnCambiaBtn2);
+    $('#correo').on('mouseover', fnCambiaBtn);
+    $('#correoA').on('mouseout', fnCambiaBtn2);
+    $('#linkedin').on('mouseover', fnCambiaBtn);
+    $('#linked').on('mouseout', fnCambiaBtn2);
+
+    function fnCambiaBtn(){
+        console.log('entre a la funcion')
+    $('#github').removeClass('visible').addClass('oculto');
+    $('#git').removeClass('oculto').addClass('visible');
+    $('#whatsapp').removeClass('visible').addClass('oculto');
+    $('#wsp').removeClass('oculto').addClass('visible');
+    $('#correo').removeClass('visible').addClass('oculto');
+    $('#correoA').removeClass('oculto').addClass('visible');
+    $('#linkedin').removeClass('visible').addClass('oculto');
+    $('#linked').removeClass('oculto').addClass('visible');
+    }
+
+    function fnCambiaBtn2(){
+        console.log('entreeeee')
+    $('#git').removeClass('visible').addClass('oculto');
+    $('#github').removeClass('oculto').addClass('visible');
+    $('#wsp').removeClass('visible').addClass('oculto');
+    $('#whatsapp').removeClass('oculto').addClass('visible');
+    $('#correoA').removeClass('visible').addClass('oculto');
+    $('#correo').removeClass('oculto').addClass('visible');
+    $('#linked').removeClass('visible').addClass('oculto');
+    $('#linkedin').removeClass('oculto').addClass('visible');
+    }
+})*/
+
+$('#github').on('mouseover', fnCambiaBtn);
+$('#git').on('mouseout', fnCambiaBtn2);
+$('#whatsapp').on('mouseover', fnCambiaBtn3);
+$('#wsp').on('mouseout', fnCambiaBtn4);
+$('#correo').on('mouseover', fnCambiaBtn5);
+$('#correoA').on('mouseout', fnCambiaBtn6);
+$('#linkedin').on('mouseover', fnCambiaBtn7);
+$('#linked').on('mouseout', fnCambiaBtn8);
+
+function fnCambiaBtn(){
+$('#github').removeClass('visible').addClass('oculto');
+$('#git').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn3(){
+    $('#whatsapp').removeClass('visible').addClass('oculto');
+    $('#wsp').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn5(){
+    $('#correo').removeClass('visible').addClass('oculto');
+    $('#correoA').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn7(){
+    $('#linkedin').removeClass('visible').addClass('oculto');
+    $('#linked').removeClass('oculto').addClass('visible');
+}
+
+function fnCambiaBtn2(){
+$('#git').removeClass('visible').addClass('oculto');
+$('#github').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn4(){
+    $('#wsp').removeClass('visible').addClass('oculto');
+    $('#whatsapp').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn6(){
+    $('#correoA').removeClass('visible').addClass('oculto');
+    $('#correo').removeClass('oculto').addClass('visible');
+}
+function fnCambiaBtn8(){
+    $('#linked').removeClass('visible').addClass('oculto');
+    $('#linkedin').removeClass('oculto').addClass('visible');
+}
